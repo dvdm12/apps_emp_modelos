5afa82d2efe6ebf7bbdfae451e5e7114ebe5c9fb
dvdm12 <dvddavid677@gmail.com>
2025-02-22 21:09:37 -0500

    i've created spring boot project with its dependencies

---
+++ src/main/java/com/example/miapp/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.miapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan(basePackages = "com.example.demo.models")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
